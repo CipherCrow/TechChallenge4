5baea8ec25cacfc6e0265b9d7ac489ca63680b9c
CipherCrow <jonastorquatro@gmail.com>
2025-02-04 01:04:23 -0300

    Adicionando estrutura básica dos webservices

---
+++ wsProdutos/src/test/java/br/com/techchallenge/safedeliver/gerenciamentoProdutos/GerenciamentoProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.techchallenge.safedeliver.gerenciamentoProdutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
